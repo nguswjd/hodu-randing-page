91de03a71ead391e6f61453031878cf896e8bc63
hyeon <nguswjd02@gmail.com>
2025-05-18 15:24:54 +0900

    jquery 코드 구현

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let subscribe = $('.user-address > button');
+let modal = $('.modal');
+let closeModal = $('.open-tabMenu');
+
+subscribe.click(function(e){
+  e.preventDefault();
+  modal.removeClass('sr-only');
+});
+
+closeModal.click(function(e){
+  e.preventDefault();
+  modal.addClass('sr-only');
+});
+
+let tabMenu = $('.mobile-tab-menu');
+let openTabMenu = $('.open-tabMenu');
+let closeTabMenu = $('.open-tabMenu div > a');
+
+tabMenu.click(function(e){
+  e.preventDefault();
+  openTabMenu.removeClass('sr-only'); 
+  setTimeout(function() {
+    openTabMenu.addClass('active'); 
+  }, 10);
+});
+
+closeTabMenu.click(function(e){
+  e.preventDefault();
+  openTabMenu.removeClass('active');
+  setTimeout(function() {
+    openTabMenu.addClass('sr-only');
+  }, 300);
+});
+
+
